26883f65e1969753129df75f29c55e7811483223
Konstantin Bondarenko <43937942+Konstantin84UKR@users.noreply.github.com>
2021-09-21 19:33:49 +0300

    ch2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const root = document.querySelector("#root");
 
 const game = new Game();
 const view = new View(root, 480, 520, 20, 10);
-//const controller = new Controller(game, view, view);
+const controller = new Controller(game, view, view);
 
 window.game = game;
 window.view = view;
